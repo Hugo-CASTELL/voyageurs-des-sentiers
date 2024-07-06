1be36eff3cb396c0403d6a1efce3b66a1b18f05c
Hugo CASTELL <hugo.castell@outlook.fr>
2024-07-06 20:45:26 +0200

    Added Leaflet and clicking to set position

--- src/pages/MeteoPage.tsx
+++ src/pages/MeteoPage.tsx
@@ -1,11 +1,14 @@
-import {useState} from "react";
+import {useMemo, useRef, useState} from "react";
 import {getMeteoWebsites, MeteoWebSite} from "../utils/MeteoWebsites.ts";
+import {MapContainer, Marker, Popup, TileLayer, useMapEvents} from "react-leaflet";
+import {LatLng, map} from "leaflet";
 
 export function MeteoPage() {
     // #--------#
     // # States #
     // #--------#
     const [isMapOpen, setIsMapOpen] = useState<boolean>(false);
+    const [mapMarkers, setMapMarkers] = useState<{lat: number,lng: number}[]>([]);
     const [latitude, setLatitude] = useState<string>("");
     const [longitude, setLongitude] = useState<string>("");
     const [results, setResults] = useState<[string, string, string][]>();
@@ -19,8 +22,40 @@ export function MeteoPage() {
     // # Get results #
     // #-------------#
     function search(): void{
-        if(latitude && longitude)
+        if(latitude && longitude){
             setResults(classes.map((website: MeteoWebSite) => {return [website.name, website.desc, website.constructUrl(latitude, longitude, '')] }));
+
+            if(mapMarkers.length <= 0){
+                let lat = parseFloat(latitude);
+                let lng = parseFloat(longitude);
+                setMapMarkers([{ lat, lng }]);
+            }
+        }
+    }
+
+    // #------------------#
+    // # Map interactions #
+    // #------------------#
+    function MapEventsHandler(){
+        const map = useMapEvents({
+            click(e) {
+                let latlng: LatLng = e.latlng;
+                if(latlng){
+                    // Get values
+                    let lat: number = latlng.lat;
+                    let lng: number = latlng.lng;
+
+                    // Set them
+                    setLatitude(lat.toString());
+                    setLongitude(lng.toString());
+
+                    // New marker on the map
+                    setMapMarkers([{ lat, lng }]);
+                    map.flyTo({lat, lng});
+                }
+            },
+        });
+        return null;
     }
 
     return (
@@ -30,6 +65,7 @@ export function MeteoPage() {
                 {/* Latitude */}
                 <div className="mb-4 w-1/4 h-full">
                     <input onChange={e => setLatitude(e.target.value)}
+                           value={latitude}
                            className="shadow appearance-none border rounded w-full h-full px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                            id="latitude" type="text" placeholder="Latitude"/>
                 </div>
@@ -37,6 +73,7 @@ export function MeteoPage() {
                 {/* Longitude */}
                 <div className="mb-4 w-1/4 h-full">
                     <input onChange={e => setLongitude(e.target.value)}
+                           value={longitude}
                            className="shadow appearance-none border rounded w-full h-full px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                            id="longitude" type="text" placeholder="Longitude"/>
                 </div>
@@ -44,7 +81,7 @@ export function MeteoPage() {
                 {/* Opening map */}
                 <button onClick={() => setIsMapOpen(!isMapOpen)}
                         type="button"
-                        className="flex justify-center items-center bg-gray-200 hover:bg-gray-300 font-semibold w-10 h-full rounded focus:outline-none focus:shadow-outline">
+                        className={"flex justify-center items-center" + ( isMapOpen ? " bg-gray-300 hover:bg-gray-200 " : " bg-gray-200 hover:bg-gray-300 ") + "font-semibold w-10 h-full rounded focus:outline-none focus:shadow-outline"}>
                     <img src={"/assets/pushpin.png"}
                          alt="Opening map"
                          className={"w-6 mx-1"}
@@ -54,15 +91,26 @@ export function MeteoPage() {
                 {/* Search */}
                 <button onClick={() => search()}
                         type="button"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-semibold h-full px-4 rounded focus:outline-none focus:shadow-outline">
+                        className={"bg-blue-500 hover:bg-blue-700 text-white font-semibold h-full px-4 rounded focus:outline-none focus:shadow-outline"}>
                             Rechercher
                 </button>
             </div>
 
-            <div>
+            <div className={"flex justify-center items-center w-full"}>
                 {isMapOpen &&
-                    <div>
-                        hello
+                    <div id="map" className={"bg-slate-200 p-2 m-5 rounded"}>
+                        <MapContainer center={[47, 1.5]} zoom={5} scrollWheelZoom={true} style={{width:'50vw', height:'50vh'}}>
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            {mapMarkers.map((marker, idx) => (
+                                <Marker key={idx}
+                                        draggable={false}
+                                        position={[marker.lat, marker.lng]} />
+                            ))}
+                            <MapEventsHandler />
+                        </MapContainer>
                     </div>
                 }
             </div>
